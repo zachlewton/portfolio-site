2bf9d8a363e4d1037a7748d83709962890cfa836
zachlewton <zach.g.lewton@gmail.com>
2021-03-18 13:39:00 -0400

    changed fonts and stuff

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,29 +1,21 @@
-import React, { Component } from "react";
-import axios from "axios";
-import style from "./About.module.css";
-import FadeIn from "react-fade-in";
+import React, { Component } from 'react';
+import axios from 'axios';
+import style from './About.module.css';
+import FadeIn from 'react-fade-in';
 export class About extends Component {
-  render() {
-    return (
-      <FadeIn transitionDuration={900} delay={300}>
-        <div className={style.about}>
-          <p>
-            Abby is a person in New York who makes films, music, and is not the
-            same Abby Lee that stars in Dance Moms. She enjoys comedy, and
-            offering commentary about many things—entertainment, politics, arts
-            & culture. Mmm, how fresh! Her laptop is running out of space
-            because she has too many random files and Glee songs in the iTunes
-            library.
-          </p>
-          <p>
-            She is proficient in Adobe Premiere, Photoshop, InDesign as well as
-            a variety of other computer apps. Technologically, she is very
-            sound.
-          </p>
-        </div>
-      </FadeIn>
-    );
-  }
+	render() {
+		return (
+			<FadeIn transitionDuration={900} delay={300}>
+				<div className={style.about}>
+					<p>
+						Abby Lee is a Los Angeles-born artist focused on graphic design,
+						illustration, and textile arts. She lives in Brooklyn, NY.
+					</p>
+					<p></p>
+				</div>
+			</FadeIn>
+		);
+	}
 }
 
 export default About;
--- src/Components/posts/Posts.jsx
+++ src/Components/posts/Posts.jsx
@@ -1,54 +1,54 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Post from "../Post/Post";
-import style from "./Posts.module.css";
-import Loading from "../Loading/Loading";
+import React, { Component } from 'react';
+import axios from 'axios';
+import Post from '../Post/Post';
+import style from './Posts.module.css';
+import Loading from '../Loading/Loading';
 
-import FadeIn from "react-fade-in";
+import FadeIn from 'react-fade-in';
 
 export class Posts extends Component {
-  state = {
-    posts: [],
-    isLoaded: false,
-    category: "",
-  };
+	state = {
+		posts: [],
+		isLoaded: false,
+		category: '',
+	};
 
-  componentDidMount() {
-    axios
-      .get("https://www.portfolioabby.com/wp-json/wp/v2/posts?per_page=100")
-      .then((res) =>
-        this.setState({
-          posts: res.data,
-          isLoaded: true,
-        })
-      )
-      .catch((err) => console.log(err));
-  }
-  render() {
-    const { posts, isLoaded } = this.state;
+	componentDidMount() {
+		axios
+			.get('https://www.portfolioabby.com/wp-json/wp/v2/posts?per_page=100')
+			.then((res) =>
+				this.setState({
+					posts: res.data,
+					isLoaded: true,
+				})
+			)
+			.catch((err) => console.log(err));
+	}
+	render() {
+		const { posts, isLoaded } = this.state;
 
-    if (isLoaded) {
-      return (
-        <div className={style.flexContainer}>
-          {posts.map((post) => (
-            <FadeIn transitionDuration={900} delay={300}>
-              <Post
-                title={post.title.rendered}
-                description={post.content.rendered}
-                subtitle={post.acf.subtitle}
-                imageSrc={post.featured_media}
-                key={post.id}
-                categories={post.categories}
-                url={post.acf.video}
-              />
-            </FadeIn>
-          ))}
-        </div>
-      );
-    }
+		if (isLoaded) {
+			return (
+				<div className={style.flexContainer}>
+					{posts.map((post) => (
+						<FadeIn transitionDuration={900} delay={300}>
+							<Post
+								title={post.title.rendered}
+								description={post.content.rendered}
+								subtitle={null}
+								imageSrc={post.featured_media}
+								key={post.id}
+								categories={post.categories}
+								url={post.acf.video}
+							/>
+						</FadeIn>
+					))}
+				</div>
+			);
+		}
 
-    return null;
-  }
+		return null;
+	}
 }
 
 export default Posts;
