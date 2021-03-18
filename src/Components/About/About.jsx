bf67e0820261fc66739579a8a91d47c91629e673
zachlewton <zach.g.lewton@gmail.com>
2021-03-18 16:01:28 -0400

    unicode fix

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import style from './About.module.css';
 import FadeIn from 'react-fade-in';
+
+import abbyPic from './abbyPic.png';
+
 export class About extends Component {
 	render() {
 		return (
 			<FadeIn transitionDuration={900} delay={300}>
 				<div className={style.about}>
+					<img alt="pic of artist " src={abbyPic} />
 					<p>
 						Abby Lee is a Los Angeles-born artist focused on graphic design,
 						illustration, and textile arts. She lives in Brooklyn, NY.
 					</p>
-					<p></p>
 				</div>
 			</FadeIn>
 		);
--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,44 +1,47 @@
-import React, { Component } from "react";
-import axios from "axios";
-import style from "./Post.module.css";
-import Video from "../video/Video.jsx";
+import React, { Component } from 'react';
+import axios from 'axios';
+import style from './Post.module.css';
+import Video from '../video/Video.jsx';
 
 export default class Post extends Component {
-  state = {
-    imgUrl: "",
-  };
-  componentDidMount() {
-    const getImgUrl = axios.get(
-      `https://www.portfolioabby.com/wp-json/wp/v2/media/${this.props.imageSrc}`
-    );
-    Promise.all([getImgUrl]).then((res) => {
-      this.setState({
-        imgUrl: res[0].data.media_details.sizes.full.source_url,
-      });
-    });
-  }
+	state = {
+		imgUrl: '',
+	};
+	componentDidMount() {
+		const getImgUrl = axios.get(
+			`https://www.portfolioabby.com/wp-json/wp/v2/media/${this.props.imageSrc}`
+		);
+		Promise.all([getImgUrl]).then((res) => {
+			this.setState({
+				imgUrl: res[0].data.media_details.sizes.full.source_url,
+			});
+		});
+	}
 
-  render() {
-    return (
-      <div className={style.post}>
-        {!this.props.categories[1] ? (
-          <img className={style.image} src={this.state.imgUrl} />
-        ) : (
-          <Video url={this.props.url} />
-        )}
+	render() {
+		return (
+			<div className={style.post}>
+				{!this.props.categories[1] ? (
+					<img alt="" className={style.image} src={this.state.imgUrl} />
+				) : (
+					<Video url={this.props.url} />
+				)}
 
-        <div className={style.header}>
-          <h1 className={style.title}>{this.props.title}</h1>
-          <div
-            className={style.subtitle}
-            dangerouslySetInnerHTML={{ __html: this.props.subtitle }}
-          />
-        </div>
-        <div
-          className={style.description}
-          dangerouslySetInnerHTML={{ __html: this.props.description }}
-        />
-      </div>
-    );
-  }
+				<div className={style.header}>
+					<h1
+						className={style.title}
+						dangerouslySetInnerHTML={{ __html: this.props.title }}
+					/>
+					<div
+						className={style.subtitle}
+						dangerouslySetInnerHTML={{ __html: this.props.subtitle }}
+					/>
+				</div>
+				<div
+					className={style.description}
+					dangerouslySetInnerHTML={{ __html: this.props.description }}
+				/>
+			</div>
+		);
+	}
 }
